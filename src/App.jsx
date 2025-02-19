ea564e559ddf127787a48239d3787c6dbd265029
Adornadowilliam2 <adornadowilliam@gmail.com>
2025-02-19 16:27:16 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import { Box, Button, Container, FormControlLabel, MenuItem, Select, TextField, Typography } from '@mui/material';
+import './App.css'
+import { CheckBox } from '@mui/icons-material';
+
+function App() {
+
+
+  return (
+    <Container sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", background: "#F5F5F5" }}>
+      <Box sx={{ p: 2, background: "#fff" }}>
+        <Typography sx={{ textAlign: "center", fontWeight: "bold", mb: 2, fontSize: "20px" }}>Create account</Typography>
+
+        {/* for username */}
+        <Box >
+          <Typography >Username</Typography>
+          <Box >
+            <TextField required size='small' helperText="Here's your generated username, remember it!" sx={{mr:2}}/>
+            <Button variant='contained' color='info'>⟳</Button>
+          </Box>
+        </Box>
+        {/* for password */}
+        <Box sx={{mt:2}}>
+          <Typography >Password</Typography>
+          <Box >
+            <TextField required size='small' fullWidth/>
+          </Box>
+          
+        </Box>
+
+        {/* for age */}
+        <Box sx={{mt:2}}>
+          <Typography >Age</Typography>
+          <Box >
+            <Select fullWidth size='small'>
+              <MenuItem>1 years 0 month 0 days</MenuItem>
+            </Select>
+          </Box>
+        </Box>
+        {/* remember the password */}
+        <FormControlLabel control={<CheckBox checked={checked} onChange={handleChange} />} label="I don't agree to the terms and condition"  />
+      </Box>
+    </Container>
+  )
+}
+
+export default App
